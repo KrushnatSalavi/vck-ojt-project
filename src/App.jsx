606c3137995b56ecacf4a32e8c0011046286e6c3
Krushnat Salavi <Krushnasalavi@gmail.com>
2025-06-23 17:47:36 +0530

    Krushnat first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './styles/Pages.css'
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import CoursePage from './pages/CoursePage';
+import ContactPage from './pages/ContactPage';
+import AddmissionPage from './pages/AddmissionPage';
+
+const App = () => {
+  return (
+    <div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/course" element={<CoursePage />} />
+            <Route path="/addmission" element={<AddmissionPage/>} />
+          </Routes>
+        </Router>
+    
+    </div>
+  );
+
+}
+
+export default App;
